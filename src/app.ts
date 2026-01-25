122be71031cdea27e1056cb336207e866dd7c56b
Aurnab Paul <105810364+aurnabnil66@users.noreply.github.com>
2026-01-25 18:27:12 +0600

    app.ts and package.json modified

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-// import userRoutes from "./routes/user.route";
 import cookieParser from "cookie-parser";
+
+import userRoutes from "@/routes/user.route";
+import authRoutes from "@/routes/auth.route";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.redirect("/api/users");
+// Health check
+app.get("/", (_req, res) => {
+  res.json({ message: "API is running..." });
 });
 
-// app.use("/api/users", userRoutes);
+// Mount routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
